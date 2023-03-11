0556ac0cedd5c58a4974c7f928aa321642aa7478
healteal <kirylshuppa@gmail.com>
2023-03-11 13:25:00 +0300

    Create project

---
+++ src/main/java/by/shuppa/zoomarket/ZooMarketApplication.java
@@ -0,0 +1,13 @@
+package by.shuppa.zoomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/shuppa/zoomarket/ZooMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shuppa.zoomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
